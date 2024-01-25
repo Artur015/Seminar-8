f81283f9b45bd0cdc69acde7798927085a3ee476
geekbrains <arttu.terestsenkov@gmail.com>
2024-01-25 21:23:12 +0200

    небольшие изменения

--- copy_file.py
+++ copy_file.py
@@ -7,12 +7,12 @@ def copy_file():
     print("Выберите файл, из которого вы хотите скопировать данные")
     data_copy, nf_copy = data_file()
 
-    # Проверяем не пуст ли копируемый файл
+    
     count_rows = len(data_copy)
     if count_rows == 0:
         print("Копируемый файл пустой!")
     else:
-        # Выбираем строку для копирования
+        
         number_row = int(input(f"Введите номер строки для копирования "
                                f"от 1 до {count_rows}: "))
         while number_row < 1 or number_row > count_rows:
@@ -20,11 +20,11 @@ def copy_file():
                                    f"Введите номер строки для копирования "
                                    f"от 1 до {count_rows}: "))
 
-        # Запоминаем выбранную строку и добавляем её в конец исходного файла
+       
         copy_row = data_copy[number_row - 1]
         data.append(copy_row)
         
-        # Пересчитываем количество строк после добавления
+        
         count_rows_new = len(data)
         
         
